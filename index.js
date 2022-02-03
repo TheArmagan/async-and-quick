9c1c050694d297c6ea8ea5faa7d13caa8c3c35c7
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-02-04 00:58:23 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const quickFind = require('./lib/quickFind');
+const quickFindIndex = require('./lib/quickFindIndex');
+const quickForEach = require('./lib/quickForEach');
+const quickMap = require('./lib/quickMap');
+const quickSort = require('./lib/quickSort');
+const chunkify = require('./lib/chunkify');
+
+module.exports = {
+  quickFind,
+  quickFindIndex,
+  quickForEach,
+  quickMap,
+  quickSort,
+  chunkify
+}
